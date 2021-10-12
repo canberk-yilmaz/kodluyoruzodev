19decf6fdcf976d0d5ea3334ae0bcad435b9036f
Canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-12 17:44:48 +0300

    Last Commit - Project Complete

---
+++ Kodluyoruz-JS-Odev2/README.md
@@ -0,0 +1,8 @@
+# Kodluyoruz To Do List Projesi
+
+![Project View](Odev2.png)
+
+- Listeye boş karakter eklenememesi için koşullar yazıldı.
+- Element eklemeye, element silmeye ve elementi tamamlandı işaretlemeye yarayan fonksiyonlar yazıldı.
+- Toast bildirimleri için gerekli kodlar yazıldı.
+- JS dosyası html dosyasına eklendi.
--- Kodluyoruz-JS-Odev2/app.js
+++ Kodluyoruz-JS-Odev2/app.js
@@ -1,47 +1,55 @@
-const inputText = document.querySelector('#task');
-const addButton = document.querySelector('#liveToastBtn');
-const list = document.querySelector('#list')
+const inputText = document.querySelector("#task");
+const addButton = document.querySelector("#liveToastBtn");
+const list = document.querySelector("#list");
 
-
-addButton.addEventListener('click', (e) => {
-    if (inputText.value === "" || inputText.value.replace(/\s/g, "").length == 0) {
-        $(".error").toast("show");
-    } else {
-        console.log(inputText.value.length);
-        e.preventDefault();
-        const newLi = document.createElement('li');
-        newLi.innerHTML = inputText.value;
-        list.appendChild(newLi);
-        $(".success").toast("show");
-
-        const newSpan = document.createElement('span');
-        newSpan.innerHTML = 'x';
-        newSpan.className = "close";
-        newLi.appendChild(newSpan);
-        closeLi();
-        toggleClass();
-    }
-
-    inputText.value = "";
-})
-
+//Function for deleting tasks with clicking X
 function closeLi() {
-    const close = document.querySelectorAll('.close');
-    for (let i = 0; i < close.length; i++) {
-        close[i].addEventListener('click', () => {
-            close[i].parentElement.style.display = 'none';
-            close[i].parentElement.remove();
-        })
-    }
+  const close = document.querySelectorAll(".close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", () => {
+      close[i].parentElement.style.display = "none";
+      close[i].parentElement.remove();
+    });
+  }
 }
 
-function toggleClass() {
-    const listItems = document.getElementsByTagName('li')
-    for (let i = 0; i < listItems.length; i++) {
-        listItems[i].addEventListener('click', () => {
-            listItems[i].classList.toggle('checked')
-        })
-    }
+//event listener add and called for toggling
+function addItem() {
+  list.addEventListener(
+    "click",
+    function (event) {
+      if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+      }
+    },
+    false
+  );
 }
 
-closeLi()
+function newItem(e) {
+  if (
+    inputText.value === "" ||
+    inputText.value.replace(/\s/g, "").length == 0
+  ) {
+    $(".error").toast("show");
+  } else {
+    //console.log(inputText.value.length); testing input
+    e.preventDefault();
+    const newLi = document.createElement("li");
+    newLi.innerHTML = inputText.value;
+    list.appendChild(newLi);
+    $(".success").toast("show");
+
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = "x";
+    newSpan.className = "close";
+    newLi.appendChild(newSpan);
+  }
+  inputText.value = "";
+  closeLi();
+}
+
+addButton.addEventListener("click", newItem);
+
+closeLi();
+addItem();
